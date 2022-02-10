8bf52910b0e6fb1127da21ebdf953b121c3e0dfa
Kyle Euman <euman_kyle@yahoo.com>
2022-02-10 15:34:23 -0500

    Added lists trying to fix sizing issue

---
+++ resources/JS/index.js
@@ -0,0 +1,12 @@
+
+
+// user clicked on the add button
+// If there is any text inside the item field, add that text to todo list
+document.getElementById('add').addEventListener('click' , function() {
+    var value=document.getElementById('item').value;
+    if (value) {
+
+        
+    }
+    
+});
